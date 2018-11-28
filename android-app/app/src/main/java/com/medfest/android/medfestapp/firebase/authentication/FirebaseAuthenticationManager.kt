af01a8f813e292460c2047fe1ec8540d85bc8fda
Anderson Melo <anderson.90@gmail.com>
2018-11-28 18:46:09 -0200

    edit FirebaseAuthenticationManager.kt

--- android-app/app/src/main/java/com/medfest/android/medfestapp/firebase/authentication/FirebaseAuthenticationManager.kt
+++ android-app/app/src/main/java/com/medfest/android/medfestapp/firebase/authentication/FirebaseAuthenticationManager.kt
@@ -22,21 +22,43 @@
 
 package com.medfest.android.medfestapp.firebase.authentication
 
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
 import javax.inject.Inject
 
 class FirebaseAuthenticationManager @Inject constructor() : FirebaseAuthenticationInterface {
 
   override fun login(email: String, password: String, onResult: (Boolean) -> Unit) {
+    authentication.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+      onResult(it.isComplete && it.isSuccessful)
+    }
   }
 
   override fun register(email: String, password: String, userName: String, onResult: (Boolean) -> Unit) {
-
+    //1
+    authentication.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+      //2
+      if (it.isComplete && it.isSuccessful) {
+        authentication.currentUser?.updateProfile(UserProfileChangeRequest
+                .Builder()
+                .setDisplayName(userName)
+                .build())
+        //3
+        onResult(true)
+      } else {
+        onResult(false)
+      }
+    }
   }
 
-  override fun getUserId(): String = ""
 
-  override fun getUserName(): String = ""
+  override fun getUserId(): String = authentication.currentUser?.uid ?: ""
+  override fun getUserName(): String = authentication.currentUser?.displayName ?: ""
 
   override fun logOut(onResult: () -> Unit) {
+    authentication.signOut()
+
+    onResult()
   }
+
 }
